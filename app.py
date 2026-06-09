19d13687a699735670e5125867e6aec62db3a5fb
Aditya Rajput <ishanandaditya@gmail.com>
2026-06-09 17:30:23 +0530

    Fixed the issue

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 import google.generativeai as genai
+
+# Load environment variables
+load_dotenv()
 
 # create  a flask app
 
 app = Flask(__name__)
 CORS(app)
 # configure the gemini api
-genai.configure(api_key="AIzaSyDLdTWJ9xP4LxemvwDVmwokEeXSkRxgOPU")
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel("gemini-2.5-flash")
 
 # create a route for the chat page as /chat
